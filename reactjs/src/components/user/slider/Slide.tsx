7568d9797fe52727133c37b9c0875c847a367a5d
huyshine <huyshine1111@gmail.com>
2023-05-15 20:59:42 +0700

    up

---
+++ reactjs/src/components/user/slider/Slide.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+type Props = {};
+
+const Slide = (props: Props) => {
+    return (
+        <section className ="slider">
+            <a href="#"><img src="https://res.cloudinary.com/do9rcgv5s/image/upload/v1669080502/bia_1_jkrk1w.png"/></a>
+        </section>
+    )
+};
+
+export default Slide;
